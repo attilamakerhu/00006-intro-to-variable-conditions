156be375c42baf9704f0aef36e23ba5e87dcd549
OolongJohnson <83778844+OolongJohnson@users.noreply.github.com>
2021-07-23 15:56:09 +0200

    .hex uploaded

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+let variable = 0
+basic.showString("Hello!")
+basic.showNumber(3)
+basic.pause(500)
+basic.showNumber(2)
+basic.pause(500)
+basic.showNumber(1)
+basic.pause(500)
+variable = 1
 basic.forever(function () {
-	
+    if (variable == 1) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(100)
+    }
 })
